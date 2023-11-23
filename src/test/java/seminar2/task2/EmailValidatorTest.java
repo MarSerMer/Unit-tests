700428f61030ad1e3f1006ed6a151f2bd29e1f23
MarSerMer <marinakms@yandex.ru>
2023-11-23 20:24:42 +0300

    Seminars 1 and 2

---
+++ src/test/java/seminar2/task2/EmailValidatorTest.java
@@ -0,0 +1,38 @@
+package seminar2.task2;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+// Напишите тесты с использованием аннотаций
+// @Test,
+// @Before,
+// @After,
+// @BeforeClass,
+// @AfterClass для проверки различных сценариев, включая правильные и неправильные адреса.
+public class EmailValidatorTest {
+
+    private static EmailValidator emailValidator;
+    @BeforeAll
+    static void setUp() {
+        emailValidator = new EmailValidator();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        emailValidator = null;
+    }
+
+    @Test
+    void isValidEmail() {
+        assertTrue(emailValidator.isValidEmail("123kofa-ioh@mm"));
+    }
+
+    @Test
+    void isNotValidEmail() {
+        assertFalse(emailValidator.isValidEmail("123kofa-ioh"));
+    }
+}

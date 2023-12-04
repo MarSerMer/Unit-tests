bda070fa632c5d9c41e820c9016171496e9850e7
MarSerMer <marinakms@yandex.ru>
2023-12-04 20:15:46 +0300

    Seminar 5

---
+++ src/test/java/seminar4/task1/IntegrationTest.java
@@ -0,0 +1,22 @@
+package seminar4.task1;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegrationTest {
+    @Test
+    void checkThemAllTogether(){
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator(1,12);
+        List<Integer> generatedNumbers = randomNumberGenerator.generateRandomNumbers(5);
+        int actualMax = Collections.max(generatedNumbers);
+        MaxNumberFinder maxNumberFinder = new MaxNumberFinder();
+        assertEquals(actualMax, maxNumberFinder.findMaxNumber(generatedNumbers));
+    }
+
+}
---
+++ src/test/java/seminar4/task2/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package seminar4.task2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserRepositoryTest {
+    UserRepository userRepository = new FakeUserRepository();
+    UserService userService = new UserService(userRepository);
+
+    @Test
+    public void getUserFullName(){
+        String result = userService.getUserFullName(1);
+        String expected = "FirstName LastName";
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void getUserFullName2(){
+        String result = userService.getUserFullName(10);
+        String expected = "User not found";
+        assertEquals(expected,result);
+    }
+
+    // Фиктивная реализация UserRepository для тестов
+    private static class FakeUserRepository extends UserRepository {
+
+        public User getUserById(int id){
+            User user = new User(1, "FirstName", "LastName");
+            if (id == 1){
+                return user;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+}
